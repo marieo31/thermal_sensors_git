77a4e0c27e3e92d27a1829dad40828ee474eb580
MariO <40508635+marieo31@users.noreply.github.com>
2019-08-02 10:35:36 -0700

    switching to VoltageRatio

--- js/thermal_sensors.js
+++ js/thermal_sensors.js
@@ -12,7 +12,29 @@ var b_calib = [-61.111, -61.111];
 // Establish the connection
 $(document).ready(function() {
         var conn = new phidget22.Connection(8989, 'localhost');
+
+        // var ch = new phidget22.VoltageRatioInput();
+        // ch.onAttach = onAttach;
+        // ch.onError = onError;
+        // // ch.onPropertyChange = propChange;
+        // ch.setIsHubPortDevice(true);
+        // ch.setHubPort(0);
+        // // ch.setChannel(0) 
+
+        // conn.connect().then(function () {
+        //     console.log('connected');
+        //     ch.open().then(function (ch) {
+        //         console.log('channel open');
+        //     }).catch(function (err) {
+        //         console.log('failed to open the channel:' + err);
+        //     });
+        // }).catch(function (err) {
+        //     alert('failed to connect to server:' + err);
+        // });;
+
+
         console.log(conn)
+        // console.log(ch)
 
         // Create the chanels
         for(i=0;i<nb_ch;i++)
@@ -27,14 +49,12 @@ $(document).ready(function() {
         for(i=0;i<nb_ch;i++)
         {
             p_vi[i].onAttach = vi_onAttach;
-            console.log("blabla")
             p_vi[i].onError = onError;
             p_vi[i].onVoltageRatioChange = vi_voltageChange;
             p_vi[i].setDeviceSerialNumber(559137);
             p_vi[i].setIsHubPortDevice(true);
             p_vi[i].setHubPort(i)
             p_vi[i].setChannel(0)    
-            console.log("blibli")  
         }
 
         // Connect the hub and the chanels
@@ -55,16 +75,102 @@ $(document).ready(function() {
         });;
         console.log(p_vi)
     });
-    
+
+
+function propChange(prop) {
+    console.log(propChange)
+    if (prop === 'DataInterval')
+        $('#di').val(this.getDataInterval());
+
+    if (prop === 'VoltageRatioChangeTrigger')
+        $('#ct').val(this.getVoltageRatioChangeTrigger());
+
+    if (prop === 'BridgeEnabled')
+        $('#enableBox').prop("checked",this.getBridgeEnabled());
+
+    if (prop === 'BridgeGain')
+        $('#gainCombo').val(this.getBridgeGain());
+
+    if (prop === 'SensorType')
+        $('#sensorCombo').val(this.getSensorType());
+}    
+function onAttach(ch) {
+	console.log(ch + ' attached');
+	// phid = ch;
+	// setLabel('attachLabel',ch.getDeviceClassName() + ' - ' + ch.getChannelClassName() + ' (Channel ' + ch.getChannel() + ')');
+	// setLabel('serialLabel',ch.getDeviceSerialNumber());
+	// setLabel('versionLabel',ch.getDeviceSKU() + ' ver.' + ch.getDeviceVersion());
+	
+	// if(ch.getDeviceClass() == phidget22.DeviceClass.VINT) 
+	// 	setLabel('hubPortLabel',ch.getHubPort());
+	// else
+	// 	setLabel('hubPortLabel','N/A');
+
+	// phid.onVoltageRatioChange = ratioChange;
+	// phid.onSensorChange = svChange;
+	// phid.onError = onError;
+    // phid.onPropertyChange = propChange;
+
+	
+	// we are forcing the initialization of the DataInterval to 512
+	// $('#di').val(phid.getDataInterval());
+	// $('#di').val(+1000);
+	// $('#ct').val(phid.getVoltageRatioChangeTrigger());
+
+	// switch(phid.getDeviceID()){
+		
+	//     case phidget22.DeviceID.PN_1046:
+	// 		phid.setBridgeGain($('#gainCombo').val());
+	// 		$('#sensorLabel').hide();
+	// 		$('#sensorCombo').hide();
+	// 		$('#sensorValue').hide();
+	// 		$('#unit').hide();
+	// 		$("#gainCombo option[value='2']").remove();
+	// 		$("#gainCombo option[value='3']").remove();
+	// 		// Adding some settings for the load cell device
+	// 		$("#gainCombo option[value='1']").remove();
+	// 		break;
+	//     case phidget22.DeviceID.PN_DAQ1500:
+	//         $('#gainCombo').val(phid.getBridgeGain());
+	// 		$('#enableLabel').hide();
+	// 		$('#enableBox').hide();
+	// 		$('#sensorLabel').hide();
+	// 		$('#sensorCombo').hide();
+	// 		$('#sensorValue').hide();
+	// 		$('#unit').hide();
+	// 		$("#gainCombo option[value='3']").remove();
+	// 		$("#gainCombo option[value='4']").remove();
+	// 		$("#gainCombo option[value='5']").remove();
+	// 		$("#gainCombo option[value='6']").remove();
+	// 		break;
+	//     case phidget22.DeviceID.PN_1065:
+    //     case phidget22.DeviceID.VOLTAGE_RATIO_INPUT_PORT:
+	//     case phidget22.DeviceID.PN_1010_1013_1018_1019:
+	//     case phidget22.DeviceID.PN_1011:
+	//     case phidget22.DeviceID.PN_1202_1203:
+	//     case phidget22.DeviceID.PN_DAQ1000:
+	// 		$('#gainCombo').hide();
+	// 		$('#gainLabel').hide();
+	// 		$('#enableLabel').hide();
+	// 		$('#enableBox').hide();
+	// 		break;
+
+	// }
+
+	// $('#ratioField').show();
+	// $('#noAttach').hide();
+	// $('#Attach').show();
+}    
 
 function vi_onAttach(ch) {
-    console.log(ch + ch.id +' attached');
-    var deviceSerialNumber = ch.getDeviceSerialNumber()
-    console.log(ch.id)
-    // ch.setDataInterval(8);
-    // ch.setVoltageChangeTrigger(0.01);
-    ch.setDataInterval(2000);
-    ch.setVoltageChangeTrigger(0);
+    console.log(ch + ' attached');
+    // console.log(ch + ch.id +' attached');
+    // // var deviceSerialNumber = ch.getDeviceSerialNumber()
+    // console.log(ch.id)
+    // // ch.setDataInterval(8);
+    // // ch.setVoltageChangeTrigger(0.01);
+    // ch.setDataInterval(2000);
+    // ch.setVoltageChangeTrigger(0);
     
 }
 
@@ -158,7 +264,8 @@ function vi_voltageChange(voltage) {
 // }
 
 function onError(arg0, arg1) {
-
+    console.log("arg0 "+arg0)
+    console.log("arg1 "+arg1)
     var d = new Date();
     $('#errorTable').append('<tr><td> ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + '.' + d.getMilliseconds() + '</td><td> 0x' + arg0.toString(16) + '</td><td>' + arg1 + '</td>');
     $("#errorField").show();
